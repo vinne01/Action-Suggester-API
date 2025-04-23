984b6a3f2bc1a98a7c6e67972c161f7e7894e6e4
vinay kumar maurya <113638115+vinne01@users.noreply.github.com>
2025-04-24 01:52:22 +0530

    Update urls.py

--- action_suggester/api/urls.py
+++ action_suggester/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import AnalyzeView
-
+# converts the class-based view into a callable function that Django can use to handle requests.
 urlpatterns = [
     path('analyze/', AnalyzeView.as_view(), name='analyze'),
 ]
